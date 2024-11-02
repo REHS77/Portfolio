44d3c4ead916cbe4ec2d00b8f3b0c513ee2b6386
REHS77 <rashedsafadii7@gmail.com>
2024-11-02 17:10:29 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
-document.querySelector('.nav-right a[href="#linkedin"]').addEventListener('click', function(event) {
-    event.preventDefault(); 
-    window.open('https://www.linkedin.com/in/rashed-alsafadi/', '_blank'); 
-  });
+function toggleMenu() {
+    const nav = document.querySelector("nav");
+    nav.classList.toggle("nav-open");
+  }
   
