cda1afe366db026a30c6c93f26b90666db88dc1f
REHS77 <rashedsafadii7@gmail.com>
2024-11-03 17:48:20 +0200

    .

--- projects.js
+++ projects.js
@@ -1,8 +1,5 @@
-
-
 function toggleMenu() {
   const nav = document.querySelector('nav');
-  const navLinks = document.querySelector('.nav-links');
   nav.classList.toggle('nav-open');
   console.log("Hamburger clicked, nav-open class toggled:", nav.classList.contains('nav-open'));
 }
